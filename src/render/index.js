ae4be0d0c5d0a043d7f388b0bdd7c74a8fb373ab
Gong Wenzhe <1955224356@qq.com>
2026-05-24 03:23:46 +0800

    feat: export all render modules

--- src/render/index.js
+++ src/render/index.js
@@ -1,3 +1,8 @@
 export { SkyRenderer } from './renderer.js';
 export { SolarSystemRenderer } from './solarSystem.js';
 export { GalaxyRenderer } from './galaxy.js';
+export { MeteorRenderer } from './meteor.js';
+export { SatelliteRenderer } from './satellite.js';
+export { AtmosphereRenderer } from './atmosphere.js';
+export { PlanetDetailsRenderer } from './planetDetails.js';
+export { PerformanceManager } from './performance.js';

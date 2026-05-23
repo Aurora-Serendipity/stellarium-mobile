2f5928c0ba773c1a8a799361bbbffe75b520fbe9
Gong Wenzhe <1955224356@qq.com>
2026-05-23 18:31:15 +0800

    chore: add src/main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// 星象馆主入口
+console.log('🌟 Stellarium Mobile 初始化中...');
+
+// 隐藏加载画面
+setTimeout(() => {
+  document.getElementById('loading').classList.add('hidden');
+}, 1000);
+
+// TODO: Phase 1-8 将逐步实现

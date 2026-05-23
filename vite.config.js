893232d345f18def861f596635c073e92925085a
Gong Wenzhe <1955224356@qq.com>
2026-05-23 18:31:10 +0800

    chore: add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/stellarium-mobile/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
+})
